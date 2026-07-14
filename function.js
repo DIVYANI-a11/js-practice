945781a7384ed35a9121b27917027512697c8369
DIVYANI-a11 <divyanihase05@gmail.com>
2026-07-14 09:57:49 -0700

    initial commit

---
+++ function.js
@@ -0,0 +1,16 @@
+// function greet(){
+//    console.log("greet") 
+// }
+// greet();
+// function hello(){
+//     console.log("hello"  +  "javascript")
+// }hello()
+// 'function  myName(name){
+//     console.log("pranav"+name)
+// }
+// myName("hase")'
+function intro(name,surname)
+{
+    console.log("My name is"+ name )
+    console.log("I am learning JavaScript" + surname)
+}intro("pranav","language")
---
+++ while.js
@@ -0,0 +1,73 @@
+// let i=1
+// while(// i<=10;i++){
+// console.log(i)
+// }
+
+// let i = 1;
+
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+// let i=2;
+// while(i<=10){
+//     console.log(i);
+//     i+=2
+// }
+// let i=1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+
+    
+// }
+// let i=10;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+// let i=1;
+// while(i<=20){
+//     console.log(i);
+//     i%2==0;
+//     i++;
+// }
+// let i =1;
+// while(i<=10){
+//     console.log(i);
+//    if( i%2===0);
+//     i++;
+// }
+// let i = 5;
+// while(i<=50){
+//     console.log(i);
+//         i =i+ 5;
+
+    
+// }
+// let i = 100
+// while(i>=10){
+//     console.log(i);
+//     i=i-10;
+    
+
+// }
+// let i=3;
+// while(i<=30){
+//     console.log(i);
+//     i=i+3
+// }
+//  
+arr =[10,40,15,90,25]
+ let largest =arr[0]
+ let i=1
+while( i<arr.length){
+    if(arr[i]>largest)
+        {
+
+       largest=arr[i]
+       
+
+    }
+i++;
+} console.log(largest)
