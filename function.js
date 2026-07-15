97ee66527e60628b4de8fde27a4d3b87e3ee595f
DIVYANI-a11 <divyanihase05@gmail.com>
2026-07-15 02:22:36 -0700

    function problem

--- function.js
+++ function.js
@@ -9,8 +9,42 @@
 //     console.log("pranav"+name)
 // }
 // myName("hase")'
-function intro(name,surname)
-{
-    console.log("My name is"+ name )
-    console.log("I am learning JavaScript" + surname)
-}intro("pranav","language")
+// function intro(name,surname)
+// {
+//     console.log("My name is"+ name )
+//     console.log("I am learning JavaScript" + surname)
+// }intro("pranav","language")
+//   function favouritelanguage(name){
+//     console.log("my favourite language is"+name)
+
+//   }favouritelanguage("javascript")
+// function introduce( name,age){
+//     console.log("my name is "+name + " and I am" +  age + "  years  old")
+
+// }introduce("pranav", 23)
+
+// function add( sum1,sum2){
+//     console.log( sum1 + sum2)
+// }
+// add(12 ,30)
+// function multiply(num1,num2){
+//     console.log(num1 * num2)
+// }multiply(5,6)
+
+// if (operator === "+") {
+//     // addition
+// } else if (operator === "-") {
+//     // subtraction
+// }
+// function calculator(num1, num2, operator) {
+//     if (operator === "+") {
+//         console.log(num1 + num2);
+//     }
+// }
+
+// calculator(10, 20, "-");
+// function calculator(num1,num2,operator){
+//     if (operator === "-"){
+//         console.log(num1-num2);
+//     }
+// }
